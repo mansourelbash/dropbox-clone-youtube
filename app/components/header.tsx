f8a167000514dd9ad6314b0a9d6d3779ed9d32f1
mansourbash <mansour@zenhr.com>
2023-12-02 21:33:44 +0300

    add clerk library for using authnication & shadecn framework for ui componentto use it on design

---
+++ app/components/header.tsx
@@ -0,0 +1,29 @@
+"use client"
+
+import Link from 'next/link'
+import React from 'react'
+import Image from 'next/image'
+import { UserButton } from '@clerk/nextjs'
+import { SignInButton, SignedOut } from '@clerk/clerk-react'
+const Header = () => {
+  return (
+    <header className='flex items-center justify-between'>
+      <Link href="/" className='flex items-center space-x-2'>
+        <div className='bg-[#0160FE] w-fit'>
+          <Image src="https://www.shareicon.net/download/2016/07/13/606936_dropbox_2048x2048.png" alt="logo" className="invert" height={50} width={50} /> 
+        </div>
+      <h1 className='font-bold text-xl'>Dropbox</h1>
+      </Link>
+     { /* theme toggle button*/}
+      <div className='px-5 flex space-x-2 items-center'>
+        <UserButton afterSignOutUrl='/' />
+        <SignedOut>
+         <SignInButton afterSignInUrl='/dashboard' mode='modal'/>
+        </SignedOut>
+
+      </div>
+    </header>
+  )
+}
+
+export default Header
